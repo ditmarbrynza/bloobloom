c42740bffc137a78f725b4eba1432327d196cd6b
ditmar <ditmarwork@gmail.com>
2022-12-18 23:22:06 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resource :frames, only: [] do
+        get :index, on: :collection
+        post :create
+      end
+      resource :lenses, only: [] do
+        get :index, on: :collection
+        post :create
+      end
+    end
+  end
 end
