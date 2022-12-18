5714cb1271b9b0797bbbae50a5b502ff347c8500
ditmar <ditmarwork@gmail.com>
2022-12-18 23:20:04 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+currencies_data = [
+  { currency_name: 'USD', currency_code: 840 },
+  { currency_name: 'GBP', currency_code: 826 },
+  { currency_name: 'EUR', currency_code: 978 },
+  { currency_name: 'JOD', currency_code: 400 },
+  { currency_name: 'JPY', currency_code: 392 }
+]
+
+Currency.create!(currencies_data)
+
+User.create!(role: :admin, email: 'admin@gmail.com', currency: Currency.first, 
+  api_token: SecureRandom.hex(15))
+User.create!(role: :client, email: 'client@gmail.com', currency: Currency.last, 
+  api_token: SecureRandom.hex(15))
