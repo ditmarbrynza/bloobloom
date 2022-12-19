47a67194b3a73f21354cfcf1c8c611ca9e757ad5
ditmar <ditmarwork@gmail.com>
2022-12-19 07:02:44 +0300

    update shopping basket logic

--- app/controllers/api/v1/shopping_basket_controller.rb
+++ app/controllers/api/v1/shopping_basket_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ShoppingBasketController < ApiController
       def index
-        @glasses = Glass.all
+        @glasses = Glass.where(user_id: @current_user.id)
         render json: @glasses, each_serializer: ::Api::V1::GlassesSerializer
       end
     end
