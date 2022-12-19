0e2cfbd15a81498e8e8939d0131aa503095f5ab2
ditmar <ditmarwork@gmail.com>
2022-12-19 07:22:25 +0300

    update auth

--- app/controllers/concerns/api/auth_handler.rb
+++ app/controllers/concerns/api/auth_handler.rb
@@ -5,13 +5,19 @@ module Api
     extend ActiveSupport::Concern
 
     NO_AUTH_TOKEN_ERROR = ::Errors::ApiError.new("You have to specify your API token.", nil, 403)
+    INVALID_AUTH_TOKEN_ERROR = ::Errors::ApiError.new("You have to specify valid API token.", nil, 
+      403)
 
     def authenticate_user!
       if (token = request.headers['Authorization']).blank?
         raise NO_AUTH_TOKEN_ERROR
       end
-
+      
       @current_user = User.find_by(api_token: token)
+      
+      unless @current_user
+        raise INVALID_AUTH_TOKEN_ERROR
+      end
     end
   end
 end
